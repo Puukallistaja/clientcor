0addae89875fc5ceee5a9b47f4a0678dcd43d51d
Arto Haponen <arto.haponen@unity.pl>
2020-04-21 21:48:24 +0200

    Add cordova

---
+++ src/pages/OfficePage.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  q-page.flex.flex-center
+    office
+</template>
+
+<script>
+import Office from "components/Office"
+export default {
+  name: 'OfficePage',
+  components: {
+    Office
+  }
+}
+</script>
